d2c30309833ffeb5b2a1045109e89da6785ed1a8
Anto <m.antoberlianto1993@gmail.com>
2022-12-28 08:48:03 +0700

    v 1.0.2
    - Add Example Request Swagger UI (ApiModelProperty)

--- src/main/java/com/mab/format/RequestBodyFormat.java
+++ src/main/java/com/mab/format/RequestBodyFormat.java
@@ -3,6 +3,7 @@ package com.mab.format;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -13,6 +14,7 @@ import lombok.ToString;
 @JsonInclude(Include.NON_NULL)
 public class RequestBodyFormat {
 
+    @ApiModelProperty(notes = "ALL Province", example = "101", required = true)
     private Integer function_code;
     private RequestDataFormat data;
 
--- src/main/java/com/mab/format/RequestDataFormat.java
+++ src/main/java/com/mab/format/RequestDataFormat.java
@@ -3,6 +3,7 @@ package com.mab.format;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -13,10 +14,19 @@ import lombok.ToString;
 @JsonInclude(Include.NON_NULL)
 public class RequestDataFormat {
 
+    @ApiModelProperty(notes = "Specific Province Name", example = "jawa barat")
     private String province_name;
+    
+    @ApiModelProperty(notes = "Specific City", example = "bandung")
     private String city;
+
+    @ApiModelProperty(notes = "Specific Sub District", example = "kiaracondong")
     private String sub_district;
+
+    @ApiModelProperty(notes = "Specific Urban", example = "BABAKAN SARI")
     private String urban;
+    
+    @ApiModelProperty(notes = "Specific Postal Code", example = "40283")
     private String postal_code;
 
 }
